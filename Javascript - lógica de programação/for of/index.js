a8e08f839e9febc64c7d360d5aecb8191d44cc31
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-02-25 17:34:34 -0300

    exer

---
+++ Javascript - lógica de programação/for of/index.js
@@ -0,0 +1,52 @@
+// todas strings e arrays possuem indices
+// const nome = 'Dionisio Samuel';
+
+// for (let i = 0; i < nome.length; i++) {
+//     console.log(nome[i]);
+// }
+
+// for (let i in nome){
+//     console.log(nome[i]);
+// }
+
+// for (let valor of nome) {
+//     console.log(valor);
+// }
+
+// const nome = ['Dionisio', 'Samuel','Dos','Santos', 'Leão'];
+
+// for (let valor of nome) {
+//         console.log(valor);
+//     }
+
+// nome.forEach(function (el) {
+//     console.log(el);
+// });
+
+// nome.forEach(function (valor, indice) {
+//     console.log(valor, indice);
+// });
+
+
+// nome.forEach(function (valor, indice, array) {
+//     console.log(valor, indice, array);
+// });
+
+const pessoa = {
+    nome : 'Dionisio',
+    sobrenome: 'Otavio'
+};
+
+// for (let chave in pessoa){
+//     console.log(chave, pessoa [chave]);
+// }
+
+for (let valor in pessoa){
+   
+}
+
+
+// for classico - geralmente com iteraveis(array ou strings)
+// for in - retorna o indice ou chave (string, array ou objetos)
+// for of - Retorna o valor em si (iteráveis, arrays ou strings) 
+
