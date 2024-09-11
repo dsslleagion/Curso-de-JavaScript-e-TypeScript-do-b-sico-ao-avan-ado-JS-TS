decaacdf0150fdef7279aad27c4c0d47a0a72ee9
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-09-11 14:29:33 -0300

    11_09

---
+++ JavaScript Tooling e ES6 Modules - modulos/babel i can use/aulaBabel/main.js
@@ -0,0 +1,23 @@
+//exemplo desse codigo sendo convertido em babel
+// const nome ='Dionisio';
+// const obj = {nome};
+// const novoObj = {...obj};
+// console.log(novoObj);
+
+// const nome ='Dionisio';
+// const obj = {nome};
+// const novoObj = {...obj};
+// console.log(novoObj);
+
+// let oi = "dionisio"
+
+
+
+class Pessoa{
+    constructor(nome,sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+
+const p1 = new Pessoa ("Dionisio","Samuel")
